2382cd91efbb96330016195aff15324df769dd8c
MDOBreno <breno.medeiros@icloud.com>
2025-03-29 02:17:43 -0300

    -Conclusão S04A25 -Conclusao sobre a aula de Api que consulta os Carros no banco de dados e devolve como json

--- carros/src/main/java/com/oliverios/carros/domain/Carro.java
+++ carros/src/main/java/com/oliverios/carros/domain/Carro.java
@@ -1,25 +1,44 @@
 package com.oliverios.carros.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "carro")
 public class Carro {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
+	@Column(name = "nome")
 	private String nome;
+	
+	public Carro() {
+		// Construtor sem parametro necessario pro spring autoCriar no formato DDL do banco
+	}
 	
 	public Carro(Long id, String nome) {
 		this.id = id;
 		this.nome = nome;
 	}
 	
+	// GetSet Id
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
+	// GetSet Nome
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 }
---
+++ carros/src/main/java/com/oliverios/carros/domain/CarroRepository.java
@@ -0,0 +1,7 @@
+package com.oliverios.carros.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarroRepository extends CrudRepository<Carro, Long>{
+	
+}
