5d5c77afbd0f36f2fba123192298ef3883d8f22b
MDOBreno <breno.medeiros@icloud.com>
2025-03-29 01:50:56 -0300

    -Conclusão S04A23 -Conclusao sobre a aula de Injecao de dependencias com assinaturas Service e Autowired pro string gerenciar as mudancas futuras do Carros no futuro

--- carros/src/main/java/com/oliverios/carros/api/CarrosController.java
+++ carros/src/main/java/com/oliverios/carros/api/CarrosController.java
@@ -2,13 +2,9 @@ package com.oliverios.carros.api;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oliverios.carros.domain.Carro;
@@ -18,7 +14,8 @@ import com.oliverios.carros.domain.CarroService;
 @RequestMapping("/api/v1/carros")
 public class CarrosController {
 	
-	private CarroService service = new CarroService();
+	@Autowired
+	private CarroService service;
 	
 	@GetMapping()
 	public List<Carro> get() {
